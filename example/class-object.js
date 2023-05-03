d2b5752d8c1ba8bc6353b40b11ad9609a1ef493e
Parameswar Mondal <param23.03@gmail.com>
2023-05-03 23:53:11 +0530

    update class object

--- example/class-object.js
+++ example/class-object.js
@@ -8,22 +8,32 @@ class User {
     }
 
     add(key, value) {
-        if (this.myStack.length === this.stackSize) {
-
-        }
+        if (this.myStack.length === this.stackSize)
+            this.myStack.shift();
         this.myStack.push({ key, value });
-        console.log('Length ', this.myStack);
+        console.log('Operation: ', this.myStack);
     }
 
     get(key) {
         const getValue = this.myStack.filter((element) => element.key === key);
-        console.log('getValue ', getValue);
+        if (getValue.length === 0) {
+            console.log('Item not found.');
+        } else {
+            console.log('getValue ', getValue);
+        }
+    }
+
+    show() {
+        console.log('Stack ', this.myStack);
     }
 
 }
 
 // Usage:
-let user = new User(5);
+let user = new User(3);
 user.add('param', 5);
 user.add('rum', 15);
-user.get('param');
+user.add('shyam', 35);
+user.add('jadu', 25);
+user.get('shyam');
+user.show();
