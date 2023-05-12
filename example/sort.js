9b5291aa759178fa4ac5c3087ed9e077c48059ad
Parameswar Mondal <param23.03@gmail.com>
2023-05-13 00:26:57 +0530

    update sort.js

--- example/sort.js
+++ example/sort.js
@@ -1,15 +1,15 @@
 'use strict';
 
 // Sort by object key value within an array
-let obj = [
+let users = [
     { name: 'john', lname: 'doe', address: 'Alaska' },
     { name: 'tom', lname: 'hopes', address: 'California' },
     { name: 'harry', address: 'Texas' },
     { name: 'param', lname: 'mondal', address: 'msd' },
     { name: 'debo', lname: 'chak', address: 'nai' },
 ];
-let result1 = obj.sort((a, b) => a.name.localeCompare(b.name)); // use inbuild function
-let result2 = obj.sort((a, b) => {
+let result1 = users.sort((a, b) => a.name.localeCompare(b.name)); // use inbuild function
+let result2 = users.sort((a, b) => {
     if (a.name > b.name) {
         return 1;
     }
